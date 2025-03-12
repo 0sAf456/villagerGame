a2b69daa0f217302e23dcef8444faa4eceebbc11
0sAf456 <ainaras448@gmail.com>
2025-03-12 16:46:48 +0200

    added the scene

--- Player.cs
+++ Player.cs
@@ -3,19 +3,19 @@ using Godot;
 
 public partial class Player : CharacterBody2D
 {
-    [Export]
-    public int Speed { get; set; } = 400;
+	[Export]
+	public int Speed { get; set; } = 400;
 
-    public void GetInput()
-    {
-        Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
-        Velocity = inputDirection * Speed;
-    }
+	public void GetInput()
+	{
+		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+		Velocity = inputDirection * Speed;
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        base._PhysicsProcess(delta);
-        GetInput();
-        MoveAndSlide();
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+		GetInput();
+		MoveAndSlide();
+	}
 }
