670ddce6933f89877cf681b722cd7301d7271708
nojus-j <167180805+nojus-j@users.noreply.github.com>
2025-03-14 12:14:53 +0200

    crude_sword_implementation
    swinging

--- Player_Movement.gd
+++ Player_Movement.gd
@@ -9,8 +9,9 @@ var Friction = 30
 var mouse_position
 
 @onready var player_animation = $AnimatedSprite2D
-@onready var rotator_object = $ObjectRotator
-@onready var weapon_object = $ObjectRotator/Weapon
+@onready var player_hitbox = $CollisionShape2D
+@onready var weapon = $Weapon
+
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
@@ -19,14 +20,15 @@ func _physics_process(delta):
 	if velocity.length() == 0:
 		player_animation.play("idle")
 	if velocity.x > 1:
-		player_animation.flip_h = false
+		player_animation.flip_h = false #flips player sprite to the right
+		weapon.position.x = abs(weapon.position.x) #flips weapon sprite when facing right
+		player_hitbox.position.x = -abs(player_hitbox.position.x) #flips hitbox right
 		player_animation.play("run")
 	if velocity.x < -1:
-		player_animation.flip_h = true
+		player_animation.flip_h = true #flips player sprite to the left
+		weapon.position.x = -abs(weapon.position.x) #flips weapon sprite when facing left
+		player_hitbox.position.x = abs(player_hitbox.position.x) #flips hitbox left
 		player_animation.play("run")
-
-func _process(delta):
-	rotator_object.look_at(get_global_mouse_position())
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
@@ -34,3 +36,6 @@ func get_input():
 	velocity.y = move_toward(velocity.y, input_direction.y * Max_Speed, Acceleration)
 	velocity.x = move_toward(velocity.x, input_direction.x * Max_Speed, Acceleration)
 	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("attack"):
+		weapon.attack()
