ec46f6ec28db5a848153b86782ec87231e3cb771
nojus-j <167180805+nojus-j@users.noreply.github.com>
2025-03-17 20:42:07 +0200

    HealthBar_1
    Health žaidėjo pasibandymui

--- entities/weapon/sword.gd
+++ entities/weapon/sword.gd
@@ -1,6 +1,14 @@
 extends Area2D
 
+var damage: = 10
 
 @onready var animation = $AnimationPlayer
 func attack():
 	animation.play("swing")
+	
+	
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("handle_hit"):
+		body.handle_hit()
